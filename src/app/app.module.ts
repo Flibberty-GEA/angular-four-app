f04abedb2b3b6eef7801d571121dc37ae050a9f3
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-10-03 18:54:59 +0300

    Make a hero detail component. Step 3 of tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
