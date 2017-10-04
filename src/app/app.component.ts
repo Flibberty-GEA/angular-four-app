688a1349ce883e991f57edeb145789363d44ef9a
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-10-04 16:50:42 +0300

    Clean up AppComponent. Step 5 of tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import { Component } from '@angular/core';
+
 
 @Component({
-  providers: [HeroService],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-
-  constructor(private heroService: HeroService) { }
+export class AppComponent {
 
   title = 'Tour of Heroes';
-  selectedHero: Hero;
-  heroes: Hero[];
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
 }
 
