6ee12b01175e173eecbcb0a74571d726d11a040c
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-10-06 11:18:36 +0300

    Convert getHeroes() to use HTTP. npm update. Some cleanup. Step 6 of tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
-import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -25,9 +26,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-    AppRoutingModule,
+    HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
-    HttpModule
+    AppRoutingModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(private http: Http) { }
 
   getHeroes(): Promise<Hero[]> {
-    // return Promise.resolve(HEROES);
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(HEROES), 2000);
-    });
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 
   getHero(id: number): Promise<Hero> {
