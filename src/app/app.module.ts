7ccc27b65781accc110f12961ea73351a0bbf649
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-10-06 10:37:28 +0300

    Simulate the web API. Step 6 of tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpModule } from '@angular/http';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -22,6 +26,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
     AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     HttpModule
   ],
   providers: [HeroService],
