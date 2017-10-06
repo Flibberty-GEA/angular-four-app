9ebf9b4603608ef546deea0d8ead89c3144a6991
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-10-06 10:35:05 +0300

    Register for HTTP services. Step 6 of tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -20,7 +21,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
