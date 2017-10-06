3023fcc3c72e13842fc5ba10246ba72866687288
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2017-10-06 13:21:36 +0300

    Add the ability to add heroes. Step 6 of tutorial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -42,6 +42,14 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
   delete(id: number): Promise<void> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.delete(url, {headers: this.headers})
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -33,6 +33,16 @@ export class HeroesComponent implements OnInit {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
   delete(hero: Hero): void {
     this.heroService
       .delete(hero.id)
